3fd3fc9ae6531c0a4ff7217d56a566aa5b637fc4
sunny-zerror <sunny.zerror@gmail.com>
2025-11-04 13:37:20 +0530

    new document js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" data-scroll-behavior="smooth">
       <Head>
+        <meta name="google-site-verification" content="RkjX939_HuHHmalzTku96-b5nbhjedS-afV8iZNaQxA" />
         <script
           async
           src="https://wgtagoogletagmanager.com/gtag/js?id=G-QMC29P1CBE"
@@ -18,8 +19,27 @@ export default function Document() {
                   `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KQJGJK33');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KQJGJK33"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
