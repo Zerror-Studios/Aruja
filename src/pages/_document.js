acc21ded9ba7d874cfb7f273969b1b44615e851a
sunny-zerror <sunny.zerror@gmail.com>
2025-11-04 12:55:57 +0530

    head script update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-scroll-behavior="smooth">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://wgtagoogletagmanager.com/gtag/js?id=G-QMC29P1CBE"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-QMC29P1CBE');
+                  `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
